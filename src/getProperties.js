a87a100ea28f835aaa5e76d3d48462b7ab337791
VinhVu <vinh.vu@eastagile.com>
2021-08-02 11:39:39 +0700

    [f] add location selection

--- src/getProperties.js
+++ src/getProperties.js
@@ -8,12 +8,16 @@ const initialProps = {
 };
 
 export default ({ presentation } = initialProps) => {
+  const { authKey } = presentation.values;
   return {
     authKey: PropTypes.oAuth('connect to Lightspeed')
       .authUrl(`${RAYDIANT_APP_LS_RETAIL_BASE_URL}/auth`)
       .verifyUrl(`${RAYDIANT_APP_LS_RETAIL_BASE_URL}/verify`, 'auth_key')
       .logoutUrl(`${RAYDIANT_APP_LS_RETAIL_BASE_URL}/logout`, 'auth_key')
       .required(),
+    locationId: PropTypes.selection('select a business location')
+      .optionsUrl(`${RAYDIANT_APP_LS_RETAIL_BASE_URL}/locationOptions?auth_key={{authKey}}`)
+      .hide(!authKey),
     duration: PropTypes.number('duration').min(5).default(120).helperText('time in seconds.'),
   };
 };
