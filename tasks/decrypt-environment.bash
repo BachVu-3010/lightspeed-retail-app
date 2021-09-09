13bb2e25d7ac45855da17c2e8381868579eeaeb3
dkim0827 <dltcm0827@gmail.com>
2021-09-09 12:51:02 -0700

    change aes keyname && env variable

--- tasks/decrypt-environment.bash
+++ tasks/decrypt-environment.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-jet decrypt docker/staging.env.encrypted docker/staging.env --key-path=mirainc_lightspeed-retail.aes
-jet decrypt docker/prod.env.encrypted docker/prod.env --key-path=mirainc_lightspeed-retail.aes
+jet decrypt docker/staging.env.encrypted docker/staging.env --key-path=mirainc_lightspeed-retail-app.aes
+jet decrypt docker/prod.env.encrypted docker/prod.env --key-path=mirainc_lightspeed-retail-app.aes
--- tasks/encrypt-environment.bash
+++ tasks/encrypt-environment.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-jet encrypt docker/staging.env docker/staging.env.encrypted --key-path=mirainc_lightspeed-retail.aes
-jet encrypt docker/prod.env docker/prod.env.encrypted --key-path=mirainc_lightspeed-retail.aes
+jet encrypt docker/staging.env docker/staging.env.encrypted --key-path=mirainc_lightspeed-retail-app.aes
+jet encrypt docker/prod.env docker/prod.env.encrypted --key-path=mirainc_lightspeed-retail-app.aes
